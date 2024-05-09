130fb66101f80c7efe432f18b25cb33c21b39d4f
Rohit Sharma <soniarohit9034@gmail.com>
2024-05-08 20:45:33 -0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,27 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+
+
+// MongoDB connection string
+const MONGO_URI = process.env.MONGO;
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
+
+
 const app = express();
 
 app.listen(3000, ()=>{
