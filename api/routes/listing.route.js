08ca4d6041e3adc6486fabfc21e263f8c3c7f6e6
Rohit Sharma <soniarohit9034@gmail.com>
2024-05-15 20:25:50 +0530

    complete update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing} from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);//verify token for that user are correct or not
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 
 export default router;
